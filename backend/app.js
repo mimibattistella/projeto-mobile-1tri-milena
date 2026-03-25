dcd7b906236d0a10c0c45e732eac8fc93beb459a
costtamell <melanidacostaleal@gmail.com>
2026-03-25 09:28:17 -0300

    App.js no backend

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta 3000");
+});
+
